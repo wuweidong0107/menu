bbdf63be6047f9d30b682709a45c1bad1e9af23f
JackWu <wuweidong0107@gmail.com>
2022-08-29 23:10:16 +0800

    working on first ver

--- src/shell.c
+++ src/shell.c
@@ -1,15 +1,27 @@
 #include <stdbool.h>
+#include <getopt.h>
 
 #include "../src/mainloop.h"
+#include "../src/io.h"
 #include "../src/shell.h"
 
 static struct {
-    bool mode;          // 1: non interactive, 0: interactive
+    bool interactive;
+    int argc;
+    char **argv;
+    struct io *input;
     const struct shell_menu *menu;
 } data;
 
-void shell_init(void)
+static const struct option main_options [] = {
+    {"help", no_argument, 0, 'h'},
+};
+
+void shell_init(int argc, char **argv)
 {
+    data.argc = argc;
+    data.argv = argv;
+    data.interactive = (argc == 0);
     mainloop_init();
 }
 
@@ -19,11 +31,30 @@ bool shell_set_menu(const struct shell_menu *menu)
         return false;
     
     data.menu = menu;
-    
+    return true;
+}
+
+static int shell_exec(int argc, char *argv[])
+{
     return true;
 }
 
 bool shell_attach(int fd)
 {
-    // TODO
+    struct io *io;
+
+    if (data.input)
+        return false;
+    
+    io = io_new(fd);
+    data.input = io;
+
+    if (data.interactive) {
+
+    } else {
+        if (shell_exec(data.argc, data.argv) < 0) {
+            return true;
+        }
+    }
+    return true;
 }
